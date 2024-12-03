4b152b52e4574df8a20a7f509af3c24be6fa1756
Henrique Delgado <delgado.ohenrique@gmail.com>
2024-12-03 14:36:19 -0300

    Initial Commit

---
+++ MDA2024/MDA2024App.swift
@@ -0,0 +1,17 @@
+//
+//  MDA2024App.swift
+//  MDA2024
+//
+//  Created by Henrique Delgado on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MDA2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MDA2024Tests/MDA2024Tests.swift
@@ -0,0 +1,16 @@
+//
+//  MDA2024Tests.swift
+//  MDA2024Tests
+//
+//  Created by Henrique Delgado on 03/12/24.
+//
+
+import Testing
+
+struct MDA2024Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
